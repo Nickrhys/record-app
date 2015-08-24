e228f3f7b28ad74cf777f5defaa93744b381845e
Nickrhys <nick@red56.uk>
2015-08-24 17:04:47 +0100

    has a record added to it

--- spec/features/record_feature_spec.rb
+++ spec/features/record_feature_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
-feature 'records' do
-  context 'with no records added' do
+feature "records" do
+  context "with no records added" do
     it "should allow you to add a record" do
       visit records_path
       expect(page).to have_content "no records added"
       expect(page).to have_link "add a record"
+    end
+  end
+
+  context "with records added" do
+    it "displays the record in index" do
+      Record.create(name: "Marquee Moon")
+      visit records_path
+      expect(page).to have_content "Marquee Moon"
+      expect(page).not_to have_content "no records added"
     end
   end
 end
