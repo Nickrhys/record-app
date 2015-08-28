1708a479dd9539b97654908f1a8198af6817324b
Nickrhys <nick@red56.uk>
2015-08-28 13:56:09 +0100

    refactor feature test page

--- spec/features/record_pages_spec.rb
+++ spec/features/record_pages_spec.rb
@@ -12,17 +12,6 @@ feature "records" do
       end
     end
 
-    context "create a record" do
-      it "allows users to enter a record" do
-        visit records_path
-        click_link "add a record"
-        fill_in "Name", with: "After The Goldrush"
-        click_button "Create Record"
-        expect(page).to have_content "After The Goldrush"
-        expect(current_path).to eq records_path
-      end
-    end
-
     context "with records added" do
 
       it "displays the record in index" do
@@ -42,36 +31,53 @@ feature "records" do
       end
     end
 
-    describe "show" do
-      it "links to edit" do
-        record = Record.create(name: "Marquee Moon")
-        visit record_path(record)
-        click_link "edit"
-        expect(current_path).to eq(edit_record_path(record))
-      end
+    it "links to new" do
+      visit records_path
+      click_link "add a record"
+      expect(current_path).to eq(new_record_path)
+    end
+  end
 
-      context 'deleting' do
-        it 'removes a record when a user clicks a delete link' do
-          record = Record.create(name: "Marquee Moon")
-          visit record_path(record)
-          click_button "Delete Marquee Moon"
-          expect(page).not_to have_content "Marquee Moon"
-          expect(current_path).to eq(records_path)
-          expect(page).to have_content 'Record deleted successfully'
-        end
+  describe "new" do
+    context "create a record" do
+      it "allows users to enter a record" do
+        visit new_record_path
+        fill_in "Name", with: "After The Goldrush"
+        click_button "Create Record"
+        expect(page).to have_content "After The Goldrush"
+        expect(current_path).to eq records_path
       end
+    end
+  end
 
+  describe "show" do
+    it "links to edit" do
+      record = Record.create(name: "Marquee Moon")
+      visit record_path(record)
+      click_link "edit"
+      expect(current_path).to eq(edit_record_path(record))
     end
 
-    describe "edit" do
-      it "changes the record name" do
+    context 'deleting' do
+      it 'removes a record when a user clicks a delete link' do
         record = Record.create(name: "Marquee Moon")
-        visit edit_record_path(record)
-        fill_in "Name", with: "Marquee Moon: 30th Anniversary edition"
-        click_button 'Update Record'
-        expect(page).to have_content "Marquee Moon: 30th Anniversary edition"
-        expect(current_path).to eq record_path(record)
+        visit record_path(record)
+        click_button "Delete Marquee Moon"
+        expect(page).not_to have_content "Marquee Moon"
+        expect(current_path).to eq(records_path)
+        expect(page).to have_content 'Record deleted successfully'
       end
+    end
+  end
+
+  describe "edit" do
+    it "changes the record name" do
+      record = Record.create(name: "Marquee Moon")
+      visit edit_record_path(record)
+      fill_in "Name", with: "Marquee Moon: 30th Anniversary edition"
+      click_button 'Update Record'
+      expect(page).to have_content "Marquee Moon: 30th Anniversary edition"
+      expect(current_path).to eq record_path(record)
     end
   end
 
