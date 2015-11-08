3b5bf283a7b2e6a90e3e05ec08a0239e72d2171a
Nickrhys <nickrhys@hotmail.com>
2015-11-08 13:21:42 +0000

    add factory girl

---
+++ spec/factories/records.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :record do | r |   
+    r.name "Marquee Moon"
+    r.id 1
+    r.artist_id 1
+  end
+end
---
+++ spec/factories/tracks.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :track do | t |   
+    t.name "See No Evil"
+    t.record_id 1
+  end
+end
